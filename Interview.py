bb3e2d25420ad9aa7da1fb42456740b64e2a5ff1
harshit_tewari <librahtiwari@gmail.com>
2018-04-15 00:06:10 +0530

    added Interview.py

---
+++ Interview.py
@@ -0,0 +1,9 @@
+print("Welcome to the Interview")
+n= str(input("Enter your Name"))
+print()
+g= float(input("Enter your CGPA"))
+print()
+if n=="Harshit" :
+ print("You have a chance! Go and Study")
+else:
+    print("Beta! Tumse na hopaayega!")
