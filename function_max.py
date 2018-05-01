0885a0699f4802bfb356b242c2ee05e0de94bb19
harshit_tewari <librahtiwari@gmail.com>
2018-05-02 00:18:19 +0530

    added function_max.py

---
+++ function_max.py
@@ -0,0 +1,7 @@
+print("Prints the largest of two numbers")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+if a>b:
+ print("The larger no. is",a)
+else:
+ print("The larger no. is",b)
