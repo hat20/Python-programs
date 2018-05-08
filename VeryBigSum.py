d042bd4db9ee7417f2b4c165ab7b93ea8435ba3b
harshit_tewari <librahtiwari@gmail.com>
2018-05-09 04:28:14 +0530

    added VeryBigSum.py

---
+++ VeryBigSum.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import os
+import sys
+
+def aVeryBigSum(n, ar):
+    s = 0
+    for num in ar:
+        s = s + num
+    return s    
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(n, ar)
+
+    f.write(str(result) + '\n')
+
+    f.close()
