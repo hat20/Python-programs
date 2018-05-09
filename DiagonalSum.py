258eaba9362e8775ddcf89a25c5d786b73edbe16
harshit_tewari <librahtiwari@gmail.com>
2018-05-10 04:33:36 +0530

    added DiagonalDifference.py

---
+++ DiagonalSum.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import os
+import sys
+
+
+def diagonalDifference(a):
+    d1 = sum([a[x][x] for x in range(n)]) 
+    d2 = sum([a[x][n-1-x] for x in range (n)])
+    return(abs(d1-d2))    
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    a = []
+
+    for _ in range(n):
+        a.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(a)
+
+    f.write(str(result) + '\n')
+
+    f.close()
